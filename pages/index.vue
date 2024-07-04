f860fcf4e5495f261040d034946191d72ce42571
ahmadilham_13 <ahmad13ilham13@gmail.com>
2024-07-04 10:14:10 +0700

    Update: product empty info

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,16 @@
     <div class="container">
         <div class="wrapper mt-2">
             <ItemsTabs v-if="Object.keys(categories).length > 0" :categories="categories" />
-            <Items v-if="Object.keys(products).length > 0" :products="products" />            
+            <Items v-if="Object.keys(products).length > 0" :products="products" />   
+            <template v-else>
+                <div class="p-8 max-w-md mx-auto text-center">
+                    <svg class="w-16 h-16 text-gray-400 mx-auto mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V5a1 1 0 00-1-1H9a1 1 0 00-1 1v6m10 0a2 2 0 012 2v2a2 2 0 01-2 2H8m8-6H8m0 0a2 2 0 01-2 2v2a2 2 0 002 2h8a2 2 0 002-2v-2a2 2 0 00-2-2m0 0V5m-4 16h-2a1 1 0 01-1-1v-1m4 0v1a1 1 0 01-1 1h-2a1 1 0 01-1-1v-1m4 0H9"></path>
+                    </svg>
+                    <h2 class="text-2xl font-semibold text-gray-700 mb-2">Data Empty</h2>
+                    <p class="text-gray-600">The category selected was hasn't product right now.</p>
+                </div>
+            </template>         
         </div>
     </div>
 </template>
@@ -17,6 +26,7 @@
     const { getCategory, getDetailCategory } = categoryProducts;
 
     const categories = ref([]);
+    const categorySelectName = ref("");
     const products = ref([]);
     const route = useRoute();
 
@@ -42,10 +52,14 @@
     })
     
     watch(() => categories.value, (value) => {
-        setCategory(route.query.category, value[0]['id']);
+        if (value.length > 0) {
+            setCategory(route.query.category, value[0]['id']);
+            categorySelectName.value = value[0]['name'];
+        }
     })
 
     watch(() => route.query.category, (value) => {
         setCategory(value, categories.value[0]['id']);
+        categorySelectName.value = categories.value[0]['name'];
     })
 </script> 
